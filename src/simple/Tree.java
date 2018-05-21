cb46d91b2fca5c12524492c77cd4f02f3a262d3c
olin <olin6520@gmail.com>
2018-05-21 10:37:20 +0800

    完成Validate Binary Search Tree

--- src/simple/Tree.java
+++ src/simple/Tree.java
@@ -2,25 +2,13 @@ package simple;
 
 public class Tree {
 
-    public static class TreeNode {
-        int val;
-        TreeNode left;
-        TreeNode right;
-
-        TreeNode(int x) {
-            val = x;
-        }
-    }
-
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(3);
-        root.left = new TreeNode(9);
-        root.left.left = null;
-        root.left.right = null;
-        root.right = new TreeNode(20);
-        root.right.left = new TreeNode(15);
-        root.right.right = new TreeNode(7);
-        System.out.println(maxDepth(root));
+        TreeNode root = new TreeNode(0);
+//        root.left = new TreeNode(1);
+//        root.right = new TreeNode(15);
+//        root.right.left = new TreeNode(6);
+//        root.right.right = new TreeNode(20);
+        System.out.println(isValidBST(root));
     }
 
     public static int maxDepth(TreeNode root) {
@@ -30,6 +18,32 @@ public class Tree {
             int leftDepth = maxDepth(root.left);
             int rightDepth = maxDepth(root.right);
             return leftDepth > rightDepth ? leftDepth + 1 : rightDepth + 1;
+        }
+    }
+
+    private static long pre = Long.MIN_VALUE;
+
+    public static boolean isValidBST(TreeNode root) {
+        return inOrder(root);
+    }
+
+    private static boolean inOrder(TreeNode root) {
+        if (root == null) return true;
+        if (!inOrder(root.left)) return false;
+        if (pre >= root.val) return false;
+        else {
+            pre = root.val;
+        }
+        return inOrder(root.right);
+    }
+
+    public static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
         }
     }
 }
