f27cc387af871beb358e77551dab8d1c87c87018
lcb <>
2018-05-24 08:35:27 +0800

    完成level order traversal

--- src/simple/Tree.java
+++ src/simple/Tree.java
@@ -1,14 +1,22 @@
 package simple;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class Tree {
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(0);
-//        root.left = new TreeNode(1);
-//        root.right = new TreeNode(15);
-//        root.right.left = new TreeNode(6);
-//        root.right.right = new TreeNode(20);
-        System.out.println(isValidBST(root));
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        //root.left.left = new TreeNode(3);
+//        root.left.right = new TreeNode(3);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(levelOrder(root));
     }
 
     public static int maxDepth(TreeNode root) {
@@ -36,6 +44,57 @@ public class Tree {
         }
         return inOrder(root.right);
     }
+
+    public static boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        else if (root.right == null && root.left == null) return true;
+        else if (root.left == null || root.right == null) return false;
+        else {
+            return inOrderTwoTree(root.left, root.right);
+        }
+    }
+
+    private static boolean inOrderTwoTree(TreeNode left, TreeNode right) {
+        if (right == null && left == null) return true;
+        else if (left == null || right == null) return false;
+
+        if (!inOrderTwoTree(left.left, right.right)) return false;
+        if (left.val != right.val) return false;
+        return inOrderTwoTree(left.right, right.left);
+    }
+
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) return result;
+        List<Integer> temp = new ArrayList<>();
+        temp.add(root.val);
+        result.add(temp);
+        List<TreeNode> queue = new LinkedList<>();
+
+        queue.add(root);
+
+        while (queue.size() > 0) {
+            List<Integer> list = new ArrayList<>();
+            List<TreeNode> tempQueue = new ArrayList<>();
+            for (TreeNode node : queue) {
+                if (node.left != null) {
+                    list.add(node.left.val);
+                    tempQueue.add(node.left);
+                }
+                if (node.right != null) {
+                    list.add(node.right.val);
+                    tempQueue.add(node.right);
+                }
+            }
+            queue = tempQueue;
+            if (!list.isEmpty()) {
+                result.add(list);
+            }
+        }
+        return result;
+
+    }
+
 
     public static class TreeNode {
         int val;
