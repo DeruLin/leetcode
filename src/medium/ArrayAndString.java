c367618ec629d7875923dfbabacb45ef6a2283b4
olin <olin6520@gmail.com>
2018-06-21 09:59:29 +0800

    完成Set Matrix Zeroes

--- src/medium/ArrayAndString.java
+++ src/medium/ArrayAndString.java
@@ -7,8 +7,8 @@ import java.util.List;
 public class ArrayAndString {
 
     public static void main(String[] args) {
-        int nums[] = new int[]{-1, 0, 1, 2, -1, -4};
-        System.out.println(threeSum(nums));
+        int nums[][] = new int[][]{{0,1,2,0},{3,4,5,2},{1,3,1,5}};
+        setZeroes(nums);
     }
 
     public static List<List<Integer>> threeSum(int[] nums) {
@@ -36,5 +36,35 @@ public class ArrayAndString {
         }
         return res;
 
+    }
+
+
+    public static void setZeroes(int[][] matrix) {
+        boolean fr = false, fc = false;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0) fr = true;
+                    if (j == 0) fc = true;
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for (int i = 1; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[i].length; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) matrix[i][j] = 0;
+            }
+        }
+        if (fr) {
+            for (int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if (fc) {
+            for (int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
     }
 }
