470dcbf9b84e5cad5f1ca443bc238ef877f95f96
olin <olin6520@gmail.com>
2020-06-20 23:45:05 +0800

    aa

--- src/DP/Middle.java
+++ src/DP/Middle.java
@@ -33,11 +33,10 @@ public class Middle {
         }
         for (int i = 1; i <= amount; i++) {
             for (int j = 1; j <= coins.length; j++) {
-                dp[i][j] = 0;
-                for (int k = 1; k <= j; k++) {
-                    if (i - coins[k - 1] >= 0)
-                        dp[i][j] += dp[i - coins[k-1]][j - 1];
-                }
+                if (i - coins[j - 1] >= 0)
+                    dp[i][j] = dp[i - coins[j - 1]][j] + dp[i][j - 1];
+                else
+                    dp[i][j] = dp[i][j - 1];
             }
         }
         return dp[amount][coins.length];
