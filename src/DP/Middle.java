a0d922cf79b25d3b7157eeacb61784f336d93704
olin <olin6520@gmail.com>
2020-07-02 00:30:15 +0800

    aa

--- src/DP/Middle.java
+++ src/DP/Middle.java
@@ -280,6 +280,29 @@ public class Middle {
         return res;
     }
 
+    //718. 最长重复子数组 https://leetcode-cn.com/problems/maximum-length-of-repeated-subarray/
+    public static int findLength(int[] A, int[] B) {
+        int[][] dp = new int[A.length + 1][B.length + 1];
+        for (int i = 0; i < A.length + 1; i++) {
+            dp[i][0] = 0;
+        }
+        for (int j = 0; j < B.length + 1; j++) {
+            dp[0][j] = 0;
+        }
+        int result = Integer.MIN_VALUE;
+        for (int i = 1; i < A.length + 1; i++) {
+            for (int j = 1; j < B.length + 1; j++) {
+                if (A[i - 1] == B[j - 1]) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = 0;
+                }
+                if (dp[i][j] > result) result = dp[i][j];
+            }
+        }
+        return result;
+    }
+
 
     private static class TreeNode {
         int val;
