e022b45f0b592c1a652b00a641bb1beba43a9714
olin <olin6520@gmail.com>
2020-07-14 16:28:18 +0800

    aa

--- src/Array/Diff.java
+++ src/Array/Diff.java
@@ -6,25 +6,35 @@ import java.util.HashSet;
 public class Diff {
 
     //128. 最长连续序列 https://leetcode-cn.com/problems/longest-consecutive-sequence/
-    public int longestConsecutive(int[] nums) {
+    public static int longestConsecutive(int[] nums) {
+        int result = 0;
         HashSet<Integer> set = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
             set.add(nums[i]);
         }
         for (int i = 0; i < nums.length; i++) {
-            int nums
+            int count = 0;
             int num = nums[i];
             if (!set.contains(num)) continue;
-            while(set.contains(num)){
-                nu
+            while (set.contains(num)) {
+                count++;
+                set.remove(num);
+                num--;
             }
+            num = nums[i] + 1;
+            while (set.contains(num)) {
+                count++;
+                set.remove(num);
+                num++;
+            }
+            result = Math.max(result, count);
         }
-
+        return result;
     }
 
 
     public static void main(String[] args) {
-        System.out.println(isStraight(new int[]{1, 2, 3, 4, 5}));
+        System.out.println(longestConsecutive(new int[]{100, 4, 200, 1, 3, 2}));
 
     }
 
